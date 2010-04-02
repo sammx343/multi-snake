d72cbc8581f4b609432e74774e84bb4490f2ceb2
patrick.hulin@gmail.com <patrick.hulin@gmail.com@8e7bf0ff-d224-87b5-3da1-aa64fd138f72>
2010-04-02 19:56:17 +0000

    Initial commit of Snake, Location, and Direction

---
+++ trunk/src/multisnake/Location.java
@@ -0,0 +1,66 @@
+/*
+ *  Copyright (C) 2010 Patrick Hulin
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package multisnake;
+
+/**
+ *
+ * @author poodimoos
+ */
+public class Location {
+    public int x;
+    public int y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Location getAdjacentLocation(Direction dir) {
+        switch(dir) {
+            case NORTH:
+                return new Location(x, y + 1);
+            case EAST:
+                return new Location(x + 1, y);
+            case SOUTH:
+                return new Location(x, y - 1);
+            case WEST:
+                return new Location(x - 1, y);
+            case NONE:
+            default:
+                return this;
+        }
+    }
+
+    public Direction getDirectionTo(Location loc) {
+        double angle = Math.atan2(loc.x - x, loc.y - y);
+
+        if((angle > 7 * Math.PI / 4) || (angle <= Math.PI / 4))
+            return Direction.EAST;
+        else if ((angle > Math.PI / 4) && (angle <= 3 * Math.PI / 4))
+            return Direction.NORTH;
+        else if ((angle > 3 * Math.PI / 4) && (angle <= 5 * Math.PI / 4))
+            return Direction.WEST;
+        else if ((angle > 5 * Math.PI / 4) && (angle <= 7 * Math.PI / 4))
+            return Direction.SOUTH;
+        else {
+            assert false : "Getting direction failed.";
+            return Direction.NONE;
+        }
+    }
+}
---
+++ trunk/src/multisnake/Snake.java
@@ -0,0 +1,91 @@
+/*
+ *  Copyright (C) 2010 Patrick Hulin
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package multisnake;
+
+import java.util.*;
+
+/**
+ *
+ * @author poodimoos
+ */
+public class Snake {
+    private class Segment {
+        private Location location;
+
+        public Segment(Location loc) {
+            location = loc;
+        }
+
+        public Location getLocation() {
+            return location;
+        }
+    }
+
+    private static int START_SEGMENTS = 4;
+
+    private LinkedList<Segment> segments;
+    private Direction dir;
+
+    public Snake(Location loc) {
+        reset(loc);
+    }
+
+    // move snake one spot
+    public void tick() {
+        Segment firstSegment = segments.getFirst();
+        Location firstLoc = firstSegment.getLocation();
+        Location newLoc = firstLoc.getAdjacentLocation(dir);
+        Segment newSegment = new Segment(newLoc);
+
+        segments.removeLast();
+        segments.addFirst(newSegment);
+    }
+
+    public void setDirection(Direction newDir) {
+        dir = newDir;
+    }
+
+    List<Location> getLocations() {
+        LinkedList<Location> locs = new LinkedList<Location>();
+        ListIterator<Segment> it = segments.listIterator();
+
+        while(it.hasNext()) {
+            Segment seg = it.next();
+
+            locs.add(seg.getLocation());
+        }
+
+        return locs;
+    }
+
+    // if snake crashed into something or just to do initial construction
+    void reset(Location loc) {
+        segments.clear();
+        for(int i = 0; i < START_SEGMENTS; i++)
+            segments.add(new Segment(loc));
+
+        dir = Direction.NONE;
+    }
+
+    void appendSegment() {
+        Segment last = segments.getLast();
+        Segment newSegment = new Segment(last.getLocation());
+        segments.add(newSegment);
+    }
+}
